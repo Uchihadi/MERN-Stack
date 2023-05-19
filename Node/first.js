0439430845a0feed56e3d55f208d2bcb49e55682
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-05-19 07:24:34 +0530

    update

---
+++ Node/first.js
@@ -0,0 +1,9 @@
+console.log("My first Node.js program");
+
+let myFunction = (message) => {
+    console.log(message);
+}
+
+myFunction("I have begun my journey using Node.js")
+
+// document.write("My first Node.js program!");
