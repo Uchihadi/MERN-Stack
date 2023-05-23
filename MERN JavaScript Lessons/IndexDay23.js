6a5f52ee97c19e2e5da16c80a226686eeb42e53c
Nurhadi Abdul Rahman <adilovescode@gmail.com>
2023-05-23 12:35:24 +0530

    Update IndexDay23.js

--- MERN JavaScript Lessons/IndexDay23.js
+++ MERN JavaScript Lessons/IndexDay23.js
@@ -11,3 +11,45 @@ function greeting (name = "anonymous", greeting = "Hello!") {
 }
 
 greeting (null, "Welcome")
+
+//forEach looping
+const anime = ["Naruto", "Goku", "Ichigo", "Luffy"]
+
+const eachcharacter = anime.forEach (
+    function(character) {
+        console.log("Name " + character)
+    }
+)
+
+console.log(eachcharacter)
+
+const filteredArray = anime.filter (
+    function(character) {
+        if (character.includes("uto")){
+            return true // Returns the data that meets condition
+        }
+        return false
+    }
+)
+
+const filteredArray2 = anime.filter (
+    function(character) {
+        if (character.length>5){
+            return true // Returns the data that meets condition
+        }
+        return false
+    }
+)
+
+const filteredArray3 = anime.filter (
+    function (character) {
+        if (character.includes("Uchiha")){
+            return true
+        }
+        return false // Returns an empty Array
+    }
+)
+
+console.log("filteredArray: ", filteredArray)
+console.log("filteredArray2: ", filteredArray2)
+console.log("filteredArray3: ", filteredArray3)
